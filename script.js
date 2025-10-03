fc133497a15e10135f0f93f22291dd1935c55786
Shounak.cs <shounakkumbhakar650@gmail.com>
2025-10-03 19:50:26 +0530

    final commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded',() => {
+document.addEventListener('DOMContentLoaded', () => {
     const cityInput = document.getElementById("city-input");
     const getWeatherBtn = document.getElementById("get-weather-btn");
     const weatherInfo = document.getElementById("weather-info");
@@ -8,7 +8,45 @@ document.addEventListener('DOMContentLoaded',() => {
     const errorMessage = document.getElementById("error-message");
 
 
-    getWeatherBtn.addEventListener('click', () => {
+    const API_KEY = "e68fde1be6985e36e9a1f38797477c69"
+
+    getWeatherBtn.addEventListener('click', async () => {
         const city = cityInput.value.trim();
+        if (!city) return ""
+
+
+        try {
+            const weatherData = await fetchWeatherData(city)
+            displayWeatherData(weatherData)
+        } catch (error) {
+            showError()
+        }
     })
-})
+
+
+    async function fetchWeatherData(city) {
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`
+        
+        const response = await fetch(url)
+
+        if(!response.ok){
+            throw new Error(`Response status: ${response.status}`)
+        }
+
+          return await response.json(); // ✅ return parsed JSON
+}
+
+    async function displayWeatherData(weatherData) {
+        cityName.textContent = weatherData.name
+        temperatureDisplay.textContent = `${weatherData.main.temp} °C`
+        descriptionDisplay.textContent = weatherData.weather[0].description
+
+        errorMessage.classList.add('hidden');
+        weatherInfo.classList.remove('hidden');
+    }
+
+        function showError() {
+        weatherInfo.classList.add('hidden')
+        errorMessage.classList.remove('hidden')
+    }
+    })
